a1ee0c34fd1805166208b078873ecbbb532161d2
Atul Sharma <53703302+Attull@users.noreply.github.com>
2023-02-02 08:02:08 +0530

    sample

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import AutoComplete from "./components/AutoComplete";
+// import './autoComplete.css';
+
+const App = () => {
+	const cityNames = [
+		"Amsterdam",
+		"Berlin",
+		"London",
+		"New York",
+		"Paris",
+		"Rome",
+		"San Francisco",
+		"Tokyo",
+		"Washington DC",
+		"Zurich",
+		"Copenhagen",
+		"Helsinki",
+		"Madrid",
+		"Reykjavik",
+		"Stockholm",
+		"Vancouver",
+		"Vienna",
+		"Zagreb",
+		"Budapest",
+		"Dublin",
+		"Hamburg",
+		"Krakow",
+		"Lisbon",
+		"Ljubljana",
+	];
+
+	return (
+		<div className="container">
+			<AutoComplete options={cityNames} />
+		</div>
+	);
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
